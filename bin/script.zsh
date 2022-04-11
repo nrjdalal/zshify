25140ecc6a72902999f55a5bac2e5e117867badc
Neeraj Dalal <admin@nrjdalal.com>
2022-04-11 21:57:50 +0530

    Mon Apr 11 21:57:50 IST 2022

--- bin/script.zsh
+++ bin/script.zsh
@@ -1,12 +1,8 @@
 #!/bin/zsh
 mkdir -p ~/.zshify
 curl -s https://raw.githubusercontent.com/nrjdalal/zshify/master/config/prompt.zsh | cat >~/.zshify/prompt.zsh
-grep "source ~/.zshify/prompt.zsh" ~/.zshrc || echo "source ~/.zshify/prompt.zsh" >>~/.zshrc
+grep "source ~/.zshify/prompt.zsh" ~/.zshrc &>/dev/null || echo "source ~/.zshify/prompt.zsh" >>~/.zshrc
+
+which brew &>/dev/null || echo "\n$(tput setaf 1)brew not installed! install via -$(tput sgr0)\n/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"\"
 
 echo "\n$(tput setaf 2)zshify enabled, to reload, run -$(tput sgr0) exec zsh"
-
-# which brew &>/dev/null && if [[ "$(uname)" == "Darwin" ]]; then
-#   brew install gh git rsync zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
-# elif [[ "$(uname)" == "Linux" ]]; then
-#   brew install gh git rsync zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
-# fi
