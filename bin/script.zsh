de4cf5ff19dfa95cc88903ead2aa4825083d8a6a
Neeraj Dalal <admin@nrjdalal.com>
2022-04-11 21:30:57 +0530

    Mon Apr 11 21:30:57 IST 2022

--- bin/script.zsh
+++ bin/script.zsh
@@ -1,9 +1,12 @@
 #!/bin/zsh
 mkdir -p ~/.zshify
 curl -s https://raw.githubusercontent.com/nrjdalal/zshify/master/config/prompt.zsh | cat >~/.zshify/prompt.zsh
-
 grep "source ~/.zshify/prompt.zsh" ~/.zshrc || echo "source ~/.zshify/prompt.zsh" >>~/.zshrc
 
-echo
-echo "zshify enabled, to reload, run -"
-echo "exec zsh"
+echo && which brew &>/dev/null && if [[ "$(uname)" == "Darwin" ]]; then
+  brew install gh git rsync zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
+elif [[ "$(uname)" == "Linux" ]]; then
+  brew install gh git rsync zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
+fi
+
+echo "\n$(tput setaf 2)zshify enabled, to reload, run -$(tput sgr0) exec zsh"
