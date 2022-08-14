43cf9945ebd875a4e3b584c028d27a6ec8883933
Neeraj Dalal <admin@nrjdalal.com>
2022-08-14 05:55:09 +0530

    Sun Aug 14 05:55:09 IST 2022

--- config/alias.zsh
+++ config/alias.zsh
@@ -1,2 +1,7 @@
+alias c="code ."
+alias cr="code -r ."
+alias o="open ."
+
+# show / hide desktop
 alias show="defaults write com.apple.finder CreateDesktop true && killall Finder"
 alias hide="defaults write com.apple.finder CreateDesktop false && killall Finder"
