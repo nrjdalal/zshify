145713ea18d43fd6afcde6807336ad4550dd950f
Neeraj Dalal <nd941z@gmail.com>
2022-08-17 10:56:55 +0530

    Update user.zsh

--- config/user.zsh
+++ config/user.zsh
@@ -1,9 +1,9 @@
-source $(brew --prefix &>/dev/null)/share/zsh-autosuggestions/zsh-autosuggestions.zsh &>/dev/null
+source $(brew --prefix 2>/dev/null)/share/zsh-autosuggestions/zsh-autosuggestions.zsh &>/dev/null
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="bg=#000000,fg=#333333"
 
-source $(brew --prefix &>/dev/null)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh &>/dev/null
+source $(brew --prefix 2>/dev/null)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh &>/dev/null
 
-source $(brew --prefix &>/dev/null)/share/zsh-history-substring-search/zsh-history-substring-search.zsh &>/dev/null
+source $(brew --prefix 2>/dev/null)/share/zsh-history-substring-search/zsh-history-substring-search.zsh &>/dev/null
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND="fg=green,bold,underline"
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND="fg=red,bold,underline"
 bindkey '^[[A' history-substring-search-up
