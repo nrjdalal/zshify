4f2a92421e48835f4987686a8c64f31eab59fb7e
Neeraj Dalal <admin@nrjdalal.com>
2025-02-05 23:34:49 +0530

    feat: script rehual

--- bin/script.zsh
+++ bin/script.zsh
@@ -3,21 +3,20 @@ mkdir -p ~/.zshify
 
 # Clone the repository
 git clone https://github.com/nrjdalal/zshify.git ~/.zshify
-
-# Source the configuration files in ~/.zshrc
-grep "source ~/.zshify/config/prompt.zsh" ~/.zshrc &>/dev/null || echo "source ~/.zshify/config/prompt.zsh" >>~/.zshrc
-grep "source ~/.zshify/config/background.zsh" ~/.zshrc &>/dev/null || echo "source ~/.zshify/config/background.zsh" >>~/.zshrc
-grep "source ~/.zshify/config/fx.zsh" ~/.zshrc &>/dev/null || echo "source ~/.zshify/config/fx.zsh" >>~/.zshrc
-grep "source ~/.zshify/config/alias.zsh" ~/.zshrc &>/dev/null || echo "source ~/.zshify/config/alias.zsh" >>~/.zshrc
-grep "source ~/.zshify/config/plugins.zsh" ~/.zshrc &>/dev/null || echo "source ~/.zshify/config/plugins.zsh" >>~/.zshrc
-grep "source ~/.zshify/config/user.zsh" ~/.zshrc &>/dev/null || echo "source ~/.zshify/config/user.zsh" >>~/.zshrc
-
-# Download the postgres script separately
 curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/main/bin/fx.sh | cat >~/.zshify/config/postgres.zsh
-grep "source ~/.zshify/config/postgres.zsh" ~/.zshrc &>/dev/null || echo "source ~/.zshify/config/postgres.zsh" >>~/.zshrc
+
+# Source the files
+for config in prompt background fx alias plugins user postgres; do
+  grep "source ~/.zshify/config/${config}.zsh" ~/.zshrc &>/dev/null || echo "source ~/.zshify/config/${config}.zsh" >>~/.zshrc
+done
 
 # Check if Homebrew is installed
-which brew &>/dev/null || echo "\n$(tput setaf 1)brew not installed! install via -$(tput sgr0)\n/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+if command -v brew &>/dev/null; then
+  echo "\n$(tput setaf 2)brew is already installed!$(tput sgr0)"
+else
+  echo "\n$(tput setaf 1)brew not installed!$(tput sgr0) Install via:"
+  echo "/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+fi
 
 echo "\n$(tput setaf 2)zshify enabled! to reload, run -$(tput sgr0) exec zsh"
 
