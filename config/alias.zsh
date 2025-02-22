76ea6f475b058024b7b7f46979d931cf34a3641c
Neeraj Dalal <admin@nrjdalal.com>
2025-02-22 07:40:19 +0530

    feat: show/hide desk icons

--- config/alias.zsh
+++ config/alias.zsh
@@ -3,3 +3,6 @@ alias c="code ."
 alias cr="code -r ."
 
 alias nanoid="npx nanoid --alphabet QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890 --size 24"
+
+alias showdesk="defaults write com.apple.finder CreateDesktop true; killall Finder;"
+alias hidedesk="defaults write com.apple.finder CreateDesktop false; killall Finder;"
