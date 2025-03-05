35d5687ff33968bf25a4790db7f681e22dd3f1d2
Neeraj Dalal <nd941z@gmail.com>
2025-03-05 12:16:45 +0530

    Update profile.zsh

--- config/profile.zsh
+++ config/profile.zsh
@@ -1,12 +1,12 @@
 MATCH_USERNAME="nrjdalal"
 
 if [[ "$USER" == "$MATCH_USERNAME" ]]; then
-  git config --global user.name "Neeraj Dalal"
-  git config --global user.email "admin@nrjdalal.com"
   git config --global init.defaultBranch "main"
   git config --global push.autoSetupRemote true
+  git config --global user.name "Neeraj Dalal"
+  git config --global user.email "admin@nrjdalal.com"
 
-  echo "Running brew update"
+  echo "Running brew update!"
   brew update
 
   brew install -q fzf oven-sh/bun/bun gh git jq nvm rsync zoxide zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
@@ -18,9 +18,7 @@ if [[ "$USER" == "$MATCH_USERNAME" ]]; then
   brew install -q mas ollama tree
   echo "Secondary formulae ensured."
 
-  brew install -q --cask applite bruno fontbase iina numi qbittorrent spotify raycast affinity-designer affinity-photo affinity-publisher
-  # may require password on initial run
-  brew install -q --cask docker
+  brew install -q --cask bruno fontbase iina numi qbittorrent spotify raycast affinity-designer affinity-photo affinity-publisher
   echo "Secondary casks ensured."
 
   echo "Running brew upgrade"
