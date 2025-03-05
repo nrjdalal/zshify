55b1b3687a89c2178324b350efcc236a81d6b6d4
Neeraj Dalal <nd941z@gmail.com>
2025-03-06 00:26:21 +0530

    Update script.zsh

--- bin/script.zsh
+++ bin/script.zsh
@@ -1,21 +1,21 @@
 #!/bin/zsh
 
-temp_dir=$(mktemp -d)
-git clone https://github.com/nrjdalal/zshify.git "$temp_dir" &>/dev/null
-curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/main/bin/fx.sh | cat >"$temp_dir/config/postgres.zsh"
-mkdir -p ~/.zshify
-rsync -a --delete "$temp_dir"/ ~/.zshify/
-rm -rf "$temp_dir"
+mkdir -p "$(mktemp -d)/config" && mkdir -p ~/.zshify/config
+
+git clone https://github.com/nrjdalal/zshify.git "$TEMP_DIR" &>/dev/null
+curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/main/bin/fx.sh | cat >"$TEMP_DIR/config/postgres.zsh"
+rsync -a --delete "$TEMP_DIR"/ ~/.zshify/
+rm -rf "$TEMP_DIR"
 
 for config in prompt background fx alias plugins user postgres; do
   grep "source ~/.zshify/config/${config}.zsh" ~/.zshrc &>/dev/null || echo "source ~/.zshify/config/${config}.zsh" >>~/.zshrc
 done
 
 echo
-echo "-------------- $(tput setaf 2)zshify successfully installed\!$(tput sgr0) --------------"
+echo "-------------- $(tput setaf 2)zshify successfully installed!$(tput sgr0) --------------"
 if ! command -v brew &>/dev/null; then
   echo
-  echo "you can't brew without the brew, right? hightly recommended\!"
+  echo "you can't brew without the brew, right? highly recommended!"
   echo
   echo "--- $(tput setaf 6)/bin/bash -c \"\$(curl -fsSL https://rdt.li/homebrew)\"$(tput sgr0) ---"
 fi
