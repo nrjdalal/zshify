23055456ae98180e0d3bb0439be80089062353e0
Neeraj Dalal <admin@nrjdalal.com>
2025-03-06 06:30:10 -0800

    chore: small tweaks 🔥 config/profile.zsh 🔥 1 file changed 13+ 1-
    config/profile.zsh

--- config/profile.zsh
+++ config/profile.zsh
@@ -2,12 +2,24 @@ MATCH_USERNAME="nrjdalal"
 
 if [[ "$USER" == "$MATCH_USERNAME" ]]; then
   echo "==> System preferences..."
-  restart_dock=false
   [[ $(defaults read com.apple.dock "autohide") != 1 ]] && defaults write com.apple.dock "autohide" -bool "true" && restart_dock=true
+  [[ $(defaults read com.apple.dock "minimize-to-application") != 1 ]] && defaults write com.apple.dock "minimize-to-application" -bool "true" && restart_dock=true
   [[ $(defaults read com.apple.dock "orientation") != "left" ]] && defaults write com.apple.dock "orientation" -string "left" && restart_dock=true
   [[ $(defaults read com.apple.dock "show-recents") != 0 ]] && defaults write com.apple.dock "show-recents" -bool "false" && restart_dock=true
   [[ $(defaults read com.apple.dock "static-only") != 1 ]] && defaults write com.apple.dock "static-only" -bool "true" && restart_dock=true
+  [[ $(defaults read com.apple.driver.AppleBluetoothMultitouch.trackpad "Clicking") != 1 ]] && defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad "Clicking" -bool "true"
+  [[ $(defaults read com.apple.finder "CreateDesktop") != 0 ]] && defaults write com.apple.finder "CreateDesktop" -bool "false" && restart_finder=true
+  [[ $(defaults read com.apple.finder "FXRemoveOldTrashItems") != 1 ]] && defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true" && restart_finder=true
+  [[ $(defaults read com.apple.finder "ShowPathbar") != 1 ]] && defaults write com.apple.finder "ShowPathbar" -bool "true" && restart_finder=true
+  [[ $(defaults read com.apple.finder "ShowStatusBar") != 1 ]] && defaults write com.apple.finder "ShowStatusBar" -bool "true" && restart_finder=true
+  [[ $(defaults read com.apple.HIToolbox "AppleFnUsageType") != 0 ]] && defaults write com.apple.HIToolbox "AppleFnUsageType" -int "0"
+  [[ $(defaults read com.apple.WindowManager "EnableTiledWindowMargins") != 0 ]] && defaults write com.apple.WindowManager "EnableTiledWindowMargins" -bool "false"
+  [[ $(defaults read NSGlobalDomain "NSAutomaticCapitalizationEnabled") != 0 ]] && defaults write NSGlobalDomain "NSAutomaticCapitalizationEnabled" -bool "false"
+  [[ $(defaults read NSGlobalDomain "NSAutomaticSpellingCorrectionEnabled") != 0 ]] && defaults write NSGlobalDomain "NSAutomaticSpellingCorrectionEnabled" -bool "false"
+  [[ $(defaults read NSGlobalDomain "WebAutomaticSpellingCorrectionEnabled") != 0 ]] && defaults write NSGlobalDomain "WebAutomaticSpellingCorrectionEnabled" -bool "false"
+
   [[ "$restart_dock" == true ]] && killall Dock
+  [[ "$restart_finder" == true ]] && killall Finder
 
   echo "==> Setting up git..."
   git config --global init.defaultBranch "main"
