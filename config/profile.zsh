26b9dfbaa901fc31b2ba416203b9a783e11c98b0
Neeraj Dalal <admin@nrjdalal.com>
2025-03-06 06:36:56 -0800

    chore: small tweaks 🔥 config/profile.zsh 🔥 1 file changed 12+ 12-
    config/profile.zsh

--- config/profile.zsh
+++ config/profile.zsh
@@ -1,7 +1,7 @@
 MATCH_USERNAME="nrjdalal"
 
 if [[ "$USER" == "$MATCH_USERNAME" ]]; then
-  echo "==> System preferences..."
+  echo && echo "==> System preferences..."
   restart_dock=false
   restart_finder=false
   [[ $(defaults read com.apple.dock "autohide") != 1 ]] && defaults write com.apple.dock "autohide" -bool "true" && restart_dock=true
@@ -22,46 +22,46 @@ if [[ "$USER" == "$MATCH_USERNAME" ]]; then
   [[ "$restart_dock" == true ]] && killall Dock
   [[ "$restart_finder" == true ]] && killall Finder
 
-  echo "==> Setting up git..."
+  echo && echo "==> Setting up git..."
   git config --global init.defaultBranch "main"
   git config --global push.autoSetupRemote true
   git config --global user.name "Neeraj Dalal"
   git config --global user.email "admin@nrjdalal.com"
 
+  echo && echo "==> Setting up brew..."
   brew analytics off && brew update
 
-  echo "==> Ensuring brew formulae..."
+  echo && echo "==> Ensuring brew formulae..."
   brew install -q fzf oven-sh/bun/bun gh git jq nvm rsync zoxide zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
 
-  echo "==> Ensuring primary casks..."
+  echo && echo "==> Ensuring primary casks..."
   brew install -q --cask google-chrome visual-studio-code
 
-  echo "==> Ensuring secondary formulae..."
+  echo && echo "==> Ensuring secondary formulae..."
   brew install -q mas ollama tree
 
-  echo "==> Ensuring secondary casks..."
+  echo && echo "==> Ensuring secondary casks..."
   brew install -q --cask bruno fontbase iina numi qbittorrent spotify raycast affinity-designer affinity-photo affinity-publisher
 
-  echo "==> Running brew upgrade..."
+  echo && echo "==> Running brew upgrade..."
   brew upgrade
 
-  echo "==> Running brew cleanup..."
+  echo && echo "==> Running brew cleanup..."
   brew cleanup --prune=1 -s
 
-  echo "==> Ensuring node..."
+  echo && echo "==> Ensuring node..."
   source "/opt/homebrew/opt/nvm/nvm.sh"
   nvm install --lts
   corepack enable pnpm
   corepack enable yarn
 
   if [[ ! -d ~/.config/karabiner/.git ]]; then
-    echo "==> Cloning karabiner configuration..."
+    echo && echo "==> Cloning karabiner configuration..."
     mkdir -p ~/.config/karabiner && cd ~/.config/karabiner && trash
     git clone https://github.com/nrjdalal/karabiner-human-config .
   fi
 
-  echo "\n==> Some manual steps if not already done...
-
+  echo && echo "==> Some manual steps if not already done...
 ---------------------------------------------
 
 brew install --cask docker karabiner-elements
