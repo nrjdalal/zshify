4e720511b56c9d578c9c3954690bdfa331eec104
Neeraj Dalal <admin@nrjdalal.com>
2025-03-07 05:05:56 +0530

    chore: small tweaks 🔥 config/profile.zsh 🔥 1 file changed 37+ 15-
    config/profile.zsh

--- config/profile.zsh
+++ config/profile.zsh
@@ -1,25 +1,47 @@
+#!/bin/zsh
+
 MATCH_USERNAME="nrjdalal"
 
 if [[ "$USER" == "$MATCH_USERNAME" ]]; then
 
   echo && echo "==> System preferences..."
+  preferences=(
+    "com.apple.AppleMultitouchTrackpad Clicking -bool 1"
+    "com.apple.dock autohide -bool 1"
+    "com.apple.dock minimize-to-application -bool 1"
+    "com.apple.dock orientation -string left"
+    "com.apple.dock show-recents -bool 0"
+    "com.apple.dock static-only -bool 1"
+    "com.apple.finder CreateDesktop -bool 0"
+    "com.apple.finder FXRemoveOldTrashItems -bool 1"
+    "com.apple.finder ShowPathbar -bool 1"
+    "com.apple.finder ShowStatusBar -bool 1"
+    "com.apple.HIToolbox AppleFnUsageType -int 0"
+    "com.apple.WindowManager EnableTiledWindowMargins -bool 0"
+    "NSGlobalDomain NSAutomaticCapitalizationEnabled -bool 0"
+    "NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool 0"
+    "NSGlobalDomain WebAutomaticSpellingCorrectionEnabled -bool 0"
+  )
+
   restart_dock=false
   restart_finder=false
-  [[ $(defaults read com.apple.AppleMultitouchTrackpad "Clicking") != 1 ]] && defaults write com.apple.AppleMultitouchTrackpad "Clicking" -bool "true"
-  [[ $(defaults read com.apple.dock "autohide") != 1 ]] && defaults write com.apple.dock "autohide" -bool "true" && restart_dock=true
-  [[ $(defaults read com.apple.dock "minimize-to-application") != 1 ]] && defaults write com.apple.dock "minimize-to-application" -bool "true" && restart_dock=true
-  [[ $(defaults read com.apple.dock "orientation") != "left" ]] && defaults write com.apple.dock "orientation" -string "left" && restart_dock=true
-  [[ $(defaults read com.apple.dock "show-recents") != 0 ]] && defaults write com.apple.dock "show-recents" -bool "false" && restart_dock=true
-  [[ $(defaults read com.apple.dock "static-only") != 1 ]] && defaults write com.apple.dock "static-only" -bool "true" && restart_dock=true
-  [[ $(defaults read com.apple.finder "CreateDesktop") != 0 ]] && defaults write com.apple.finder "CreateDesktop" -bool "false" && restart_finder=true
-  [[ $(defaults read com.apple.finder "FXRemoveOldTrashItems") != 1 ]] && defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true" && restart_finder=true
-  [[ $(defaults read com.apple.finder "ShowPathbar") != 1 ]] && defaults write com.apple.finder "ShowPathbar" -bool "true" && restart_finder=true
-  [[ $(defaults read com.apple.finder "ShowStatusBar") != 1 ]] && defaults write com.apple.finder "ShowStatusBar" -bool "true" && restart_finder=true
-  [[ $(defaults read com.apple.HIToolbox "AppleFnUsageType") != 0 ]] && defaults write com.apple.HIToolbox "AppleFnUsageType" -int "0"
-  [[ $(defaults read com.apple.WindowManager "EnableTiledWindowMargins") != 0 ]] && defaults write com.apple.WindowManager "EnableTiledWindowMargins" -bool "false"
-  [[ $(defaults read NSGlobalDomain "NSAutomaticCapitalizationEnabled") != 0 ]] && defaults write NSGlobalDomain "NSAutomaticCapitalizationEnabled" -bool "false"
-  [[ $(defaults read NSGlobalDomain "NSAutomaticSpellingCorrectionEnabled") != 0 ]] && defaults write NSGlobalDomain "NSAutomaticSpellingCorrectionEnabled" -bool "false"
-  [[ $(defaults read NSGlobalDomain "WebAutomaticSpellingCorrectionEnabled") != 0 ]] && defaults write NSGlobalDomain "WebAutomaticSpellingCorrectionEnabled" -bool "false"
+
+  for pref in "${preferences[@]}"; do
+    domain=$(echo "$pref" | cut -d' ' -f1)
+    key_name=$(echo "$pref" | cut -d' ' -f2)
+    type=$(echo "$pref" | cut -d' ' -f3)
+    value=$(echo "$pref" | cut -d' ' -f4)
+    current_value=$(defaults read "$domain" "$key_name" 2>/dev/null)
+
+    if [[ "$current_value" != "$value" ]]; then
+      updated_value=$(defaults write "$domain" "$key_name" $type "$value" && defaults read "$domain" "$key_name")
+      echo "==> Updated $domain $key_name from $current_value to $updated_value"
+    fi
+
+    [[ "$domain" == "com.apple.dock" ]] && restart_dock=true
+    [[ "$domain" == "com.apple.finder" ]] && restart_finder=true
+  done
+
   [[ "$restart_dock" == true ]] && killall Dock
   [[ "$restart_finder" == true ]] && killall Finder
 
