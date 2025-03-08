6c75d1c504076c974ded17ffaba108827bd01a9a
Neeraj Dalal <admin@nrjdalal.com>
2025-03-08 17:07:13 +0530

    chore: small tweaks to fx.zsh ~ 1 file changed 2-
    config/fx.zsh

--- config/fx.zsh
+++ config/fx.zsh
@@ -20,8 +20,6 @@ g() {
   [[ "$commit_message" != *:* ]] && commit_message="chore: $commit_message"
   if [[ $(echo "$commit_message ~ $diff_summary" | wc -w) -gt 100 ]]; then
     commit_message="$commit_message"
-  elif [[ $(echo "$commit_message ~ $diff_summary" | wc -w) -gt 88 ]]; then
-    commit_message="$commit_message ~ $diff_summary"
   else
     commit_message=$(echo "$commit_message |~ $diff_summary|to $files_list" | cut -c1-98)
     [[ ${#commit_message} -eq 98 ]] && commit_message="$commit_message... "
