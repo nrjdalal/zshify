67590663f55f4dd5c994c2bfa36170744e1fabfa
Neeraj Dalal <admin@nrjdalal.com>
2025-05-18 00:25:21 +0530

    chore: tweaks > alias.zsh
    config/alias.zsh

--- config/alias.zsh
+++ config/alias.zsh
@@ -11,7 +11,20 @@ alias hidedesk="defaults write com.apple.finder CreateDesktop false; killall Fin
 # npm
 alias ai="bunx dalal@latest ollama"
 alias nanoid="bunx nanoid@latest --alphabet QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890 --size 24"
-alias next="bunx gitpick@latest nrjdalal/awesome-templates/tree/main/next.js-apps/next.js-pro . && bun i && git init && git add . && git commit -S -m 'feat: init awesomeness'"
 alias pick="bunx gitpick@latest"
 alias ts="bunx gitpick@latest nrjdalal/the-typescript-starter ."
 alias ui="bunx shadcn@latest add -o https://dub.sh/ui.json && bunx prettier --write --ignore-unknown *"
+
+next() {
+  bunx gitpick@latest nrjdalal/awesome-templates/tree/main/next.js-apps/next.js-pro .
+  bun i
+  bunx fx '({
+    ...this,
+    name: require("path").basename(process.cwd())
+  })' package.json >package.tmp.json && mv package.tmp.json package.json
+  if ! git rev-parse --show-toplevel >/dev/null 2>&1; then
+    git init
+    git add .
+    git commit -S -m "feat: init awesomeness"
+  fi
+}
